8e0b8ccab84eca1b040ae8fa3ce09ff140fd87ad
Fahim Al Islam <fahim6668@gmail.com>
2025-06-19 00:16:31 +0600

    White label

--- gulpfile.js
+++ gulpfile.js
@@ -455,7 +455,7 @@ gulp.task("emoji", function(cb) {
 });
 
 gulp.task("conf", function() {
-    return gulp.src(["conf/conf.example.json"])
+    return gulp.src(["conf/conf.json"])
         .pipe(gulp.dest(paths.dist));
 });
 
